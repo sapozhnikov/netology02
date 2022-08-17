3ad5b8e7733325d9e8f301785b8bc4dfd576e560
Dmitriy Sapozhnikov <aurariel@gmail.com>
2022-08-17 20:30:38 +0300

    commit #2

---
+++ src/GitDemo.java
@@ -0,0 +1,7 @@
+public class GitDemo {
+    public static void main(String[] args) {
+        int[] sales = {1, 1, 2, 7, 4, 2};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println("Max sales is " + salesManager.max());
+    }
+}
